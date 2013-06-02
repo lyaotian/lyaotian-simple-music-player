75ce32ac4ddb2c8c29507fe00702876419a4a5f5
lyaotian@gmail.com <lyaotian@gmail.com@7d031e94-b481-2a10-df11-81e43fbc7a78>
2013-06-02 07:50:05 +0000

    Exclude from VCS

---
+++ trunk/SimpleMusicPlayer/trunk/SimpleMusicPlayer/src/main/java/cn/lyaotian/simple/music/ui/fragment/MusicFragment.java
@@ -0,0 +1,41 @@
+package test.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import cn.lyaotian.simple.music.R;
+
+/**
+ * Created by lyaotian on 5/29/13.
+ */
+public class MusicFragment extends Fragment {
+    public static final String TAG = "MusicFragment";
+
+    public MusicFragment(){
+
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_music, null);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
