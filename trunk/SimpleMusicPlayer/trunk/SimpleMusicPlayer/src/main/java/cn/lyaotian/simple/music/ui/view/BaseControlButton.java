bfeff31e19a9b0ad9c8c309e094d12d9dc669cd0
lyaotian@gmail.com <lyaotian@gmail.com@7d031e94-b481-2a10-df11-81e43fbc7a78>
2013-06-07 02:13:18 +0000

    Failed commit: Default (2)

--- trunk/SimpleMusicPlayer/trunk/SimpleMusicPlayer/src/main/java/cn/lyaotian/simple/music/ui/view/BaseControlButton.java
+++ trunk/SimpleMusicPlayer/trunk/SimpleMusicPlayer/src/main/java/cn/lyaotian/simple/music/ui/view/BaseControlButton.java
@@ -26,7 +26,8 @@ public abstract class BaseControlButton extends View {
     protected int width;
     protected int height;
     protected Paint circlePaint;
-    protected ColorStateList colorStateList;
+    protected ColorStateList mForegroundColor;
+    protected int mCurForegroundColor;
 
     public BaseControlButton(Context context) {
         super(context);
@@ -49,7 +50,8 @@ public abstract class BaseControlButton extends View {
         width = resources.getDimensionPixelSize(R.dimen.control_button_default_size);
         height = width;
 
-        colorStateList = resources.getColorStateList(R.drawable.color_control_button);
+        mForegroundColor = resources.getColorStateList(R.drawable.color_control_button);
+        setForegroundColor(mForegroundColor);
 
         circlePaint = new Paint();
         circlePaint.setAntiAlias(true);
@@ -64,9 +66,18 @@ public abstract class BaseControlButton extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        circlePaint.setColor(getCurrentColor());
+        circlePaint.setColor(mCurForegroundColor);
 
         drawCircle(canvas);
+    }
+
+    public void setForegroundColor(ColorStateList colors){
+        if (colors == null) {
+            throw new NullPointerException();
+        }
+
+        mForegroundColor = colors;
+        updateTextColors();
     }
 
     private void drawCircle(Canvas canvas) {
@@ -77,8 +88,8 @@ public abstract class BaseControlButton extends View {
 
     protected int getCurrentColor() {
         int color = DEFAULT_COLOR;
-        if(colorStateList != null){
-            color = colorStateList.getColorForState(getDrawableState(), DEFAULT_COLOR);
+        if(mForegroundColor != null){
+            color = mForegroundColor.getColorForState(getDrawableState(), DEFAULT_COLOR);
             Log.d(TAG, "getCurrentColor color=" + color + "; white=" + DEFAULT_COLOR);
         }
         return color;
@@ -133,86 +144,101 @@ public abstract class BaseControlButton extends View {
         super.onLayout(changed, left, top, right, bottom);
     }
 
+    private void updateTextColors() {
+        boolean inval = false;
+        int color = mForegroundColor.getColorForState(getDrawableState(), 0);
+        if (color != mCurForegroundColor) {
+            mCurForegroundColor = color;
+            inval = true;
+        }
+
+        if (inval) {
+            invalidate();
+        }
+    }
+
     @Override
     protected void drawableStateChanged() {
         super.drawableStateChanged();
-
-        int [] drawableState = getDrawableState();
-        invalidate();
-
-        for(int state : drawableState){
-            switch (state){
-                case android.R.attr.state_focused:
-                    Log.e(TAG, "state_focused");
-                    break;
-                case android.R.attr.state_selected:
-                    Log.e(TAG, "state_selected");
-                    break;
-                case android.R.attr.state_pressed:
-                    Log.e(TAG, "state_pressed");
-                    break;
-                case android.R.attr.state_checked:
-                    Log.e(TAG, "state_checked");
-                    break;
-                case android.R.attr.state_above_anchor:
-                    Log.e(TAG, "state_above_anchor");
-                    break;
-                case android.R.attr.state_accelerated:
-                    Log.e(TAG, "state_accelerated");
-                    break;
-                case android.R.attr.state_activated:
-                    Log.e(TAG, "state_activated");
-                    break;
-                case android.R.attr.state_active:
-                    Log.e(TAG, "state_active");
-                    break;
-                case android.R.attr.state_checkable:
-                    Log.e(TAG, "state_checkable");
-                    break;
-                case android.R.attr.state_drag_can_accept:
-                    Log.e(TAG, "state_drag_can_accept");
-                    break;
-                case android.R.attr.state_drag_hovered:
-                    Log.e(TAG, "state_drag_hovered");
-                    break;
-                case android.R.attr.state_empty:
-                    Log.e(TAG, "state_empty");
-                    break;
-                case android.R.attr.state_enabled:
-                    Log.e(TAG, "state_enabled");
-                    break;
-                case android.R.attr.state_expanded:
-                    Log.e(TAG, "state_expanded");
-                    break;
-                case android.R.attr.state_first:
-                    Log.e(TAG, "state_first");
-                    break;
-                case android.R.attr.state_hovered:
-                    Log.e(TAG, "state_hovered");
-                    break;
-                case android.R.attr.state_last:
-                    Log.e(TAG, "state_last");
-                    break;
-                case android.R.attr.state_long_pressable:
-                    Log.e(TAG, "state_long_pressable");
-                    break;
-                case android.R.attr.state_middle:
-                    Log.e(TAG, "state_middle");
-                    break;
-                case android.R.attr.state_multiline:
-                    Log.e(TAG, "state_multiline");
-                    break;
-                case android.R.attr.state_single:
-                    Log.e(TAG, "state_single");
-                    break;
-                case android.R.attr.state_window_focused:
-                    Log.e(TAG, "state_window_focused");
-                    break;
-                default:
-                    Log.e(TAG, "unknow state");
-            }
+        if (mForegroundColor != null && mForegroundColor.isStateful()) {
+            updateTextColors();
+            return;
         }
-        Log.i(TAG, "----------------------------");
-        invalidate();
+
+//        int [] drawableState = getDrawableState();
+//
+//        for(int state : drawableState){
+//            switch (state){
+//                case android.R.attr.state_focused:
+//                    Log.e(TAG, "state_focused");
+//                    break;
+//                case android.R.attr.state_selected:
+//                    Log.e(TAG, "state_selected");
+//                    break;
+//                case android.R.attr.state_pressed:
+//                    Log.e(TAG, "state_pressed");
+//                    break;
+//                case android.R.attr.state_checked:
+//                    Log.e(TAG, "state_checked");
+//                    break;
+//                case android.R.attr.state_above_anchor:
+//                    Log.e(TAG, "state_above_anchor");
+//                    break;
+//                case android.R.attr.state_accelerated:
+//                    Log.e(TAG, "state_accelerated");
+//                    break;
+//                case android.R.attr.state_activated:
+//                    Log.e(TAG, "state_activated");
+//                    break;
+//                case android.R.attr.state_active:
+//                    Log.e(TAG, "state_active");
+//                    break;
+//                case android.R.attr.state_checkable:
+//                    Log.e(TAG, "state_checkable");
+//                    break;
+//                case android.R.attr.state_drag_can_accept:
+//                    Log.e(TAG, "state_drag_can_accept");
+//                    break;
+//                case android.R.attr.state_drag_hovered:
+//                    Log.e(TAG, "state_drag_hovered");
+//                    break;
+//                case android.R.attr.state_empty:
+//                    Log.e(TAG, "state_empty");
+//                    break;
+//                case android.R.attr.state_enabled:
+//                    Log.e(TAG, "state_enabled");
+//                    break;
+//                case android.R.attr.state_expanded:
+//                    Log.e(TAG, "state_expanded");
+//                    break;
+//                case android.R.attr.state_first:
+//                    Log.e(TAG, "state_first");
+//                    break;
+//                case android.R.attr.state_hovered:
+//                    Log.e(TAG, "state_hovered");
+//                    break;
+//                case android.R.attr.state_last:
+//                    Log.e(TAG, "state_last");
+//                    break;
+//                case android.R.attr.state_long_pressable:
+//                    Log.e(TAG, "state_long_pressable");
+//                    break;
+//                case android.R.attr.state_middle:
+//                    Log.e(TAG, "state_middle");
+//                    break;
+//                case android.R.attr.state_multiline:
+//                    Log.e(TAG, "state_multiline");
+//                    break;
+//                case android.R.attr.state_single:
+//                    Log.e(TAG, "state_single");
+//                    break;
+//                case android.R.attr.state_window_focused:
+//                    Log.e(TAG, "state_window_focused");
+//                    break;
+//                default:
+//                    Log.e(TAG, "unknow state");
+//            }
+//        }
+//        Log.i(TAG, "----------------------------");
     }
 }
